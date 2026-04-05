25f5163234a00e123c33a927df4ccc4548785fe7
Paulo Gutiérrez <paulo.gutierrez.s.98@gmail.com>
2026-04-05 15:55:18 -0400

    Se implementa consumo de OpenWeather API y uso de localStorage

--- assets/js/classes/GestorTareas.js
+++ assets/js/classes/GestorTareas.js
@@ -1,31 +1,50 @@
+import Tarea from "./Tarea.js";
+
 // Crear clase GestorTareas que administre tareas
 class GestorTareas {
-    constructor(){
-        this.tareas = []
-    }
+  constructor() {
+    this.tareas =
+      JSON.parse(localStorage.getItem("tareas"))?.map(
+        (tarea) =>
+          new Tarea(
+            tarea.id,
+            tarea.descripcion,
+            tarea.estado,
+            tarea.fechaCreacion,
+            tarea.fechaLimite,
+          ),
+      ) || [];
+  }
 
-    // CRUD -> Create, Read, Update, Delete
-    agregarTarea = (tarea) =>{
-        this.tareas.push(tarea)
+  // CRUD -> Create, Read, Update, Delete
+  agregarTarea = (tarea) => {
+    this.tareas.push(tarea);
+    this.#guardarTareas();
+  };
+  eliminarTarea = (id) => {
+    this.tareas = this.tareas.filter((tarea) => tarea.id !== id);
+    console.log(`Tarea con ID ${id} eliminada exitosamente.`);
+    this.#guardarTareas();
+  };
+  listarTareas = () => {
+    return this.tareas;
+  };
+  #buscarTarea = (id) => {
+    return this.tareas.find((tarea) => tarea.id === id);
+  };
+  cambiarEstado = (id) => {
+    let tarea = this.#buscarTarea(id);
+    if (!tarea) {
+      console.error("No se pudo actualizar: Tarea no encontrada.");
+      return "No se pudo actualizar: Tarea no encontrada.";
     }
-    eliminarTarea = (id) =>{
-        this.tareas = this.tareas.filter(tarea => tarea.id !== id)
-        console.log(`Tarea con ID ${id} eliminada exitosamente.`)
-    }
-    listarTareas = () =>{
-        return this.tareas
-    }
-    #buscarTarea = (id) =>{
-        return this.tareas.find(tarea => tarea.id === id)
-    }
-    cambiarEstado = (id) =>{
-        let tarea = this.#buscarTarea(id)
-        if(!tarea){
-            console.error('No se pudo actualizar: Tarea no encontrada.')
-            return 'No se pudo actualizar: Tarea no encontrada.'
-        }
-        tarea.cambiarEstado()
-        console.log(`Tarea con ID ${id} actualizada exitosamente.`)
-    }
+    tarea.cambiarEstado();
+    console.log(`Tarea con ID ${id} actualizada exitosamente.`);
+    this.#guardarTareas();
+  };
+
+  #guardarTareas = () => {
+    localStorage.setItem("tareas", JSON.stringify(this.tareas));
+  };
 }
 export default GestorTareas;
